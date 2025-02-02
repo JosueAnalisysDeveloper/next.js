f6a290f94236148bad25fe007d46d549bdca6bef
josué <josueematos15@gmail.com>
2025-02-02 12:00:39 -0300

    jaja eu termino

---
+++ project-next/src/app/product/[productId]/page.tsx
@@ -0,0 +1,7 @@
+export default function productId ( params ) {
+    return (
+        <div>
+            <h1>{params.PRODUCTID}</h1>
+        </div>
+    )
+}
---
+++ project-next/src/app/product/page.tsx
@@ -0,0 +1,7 @@
+export default function product ( params ) {
+    return (
+        <div>
+            <h1>produto {params.PRODUCTID}</h1>
+        </div>
+    )
+}
