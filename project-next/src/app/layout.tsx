c4444de22c91fb45c1a2939d49ccba2b6f2b850f
josué <josueematos15@gmail.com>
2025-02-15 10:34:22 -0300

    .

--- project-next/src/app/layout.tsx
+++ project-next/src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>cabecario da pagina</header>
         {children}
+        <footer>rodapé da pagina</footer>
       </body>
     </html>
   );
